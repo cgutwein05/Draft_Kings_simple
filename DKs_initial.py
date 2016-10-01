bc6d6b3c48a63dd89913315df6694f32f2fd14a9
cgutwein05 <cgutwein05@gmail.com>
2016-10-01 09:11:40 -0600

    Update DKs_initial.py

--- DKs_initial.py
+++ DKs_initial.py
@@ -1,5 +1,16 @@
 import pandas ## import pandas library
 
-DK_data = pandas.read_csv(raw_input("Enter reference file name: ")) ## prompt user to enter file name to be analyzed
+DK_data = pandas.read_csv("..\Draft_Kings_Salaries_WK4SunMon.csv") ## read .csv and define datafile
 
+print DK_data.head(5) ## Show the first 5 lines
+print DK_data.dtypes ## List of data types for each field
+print DK_data.shape ## size of dataset
+print DK_data['Salary'].describe(include='all') ## key statistics for Salary field
+
+DK_data_RB = DK_data[DK_data['Position']=='RB'] ## define RB data
+print DK_data_RB['Salary'].describe(include='all') ## key statistics for Salary field
+print DK_data_RB.head(10) ## top 10 RB in list
+
+DK_data_RB_1 = DK_data_RB.sort_values(by='AvgPointsPerGame', ascending=False)
+print DK_data_RB_1['Name'].head(10)
 
